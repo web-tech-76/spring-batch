7f0e9bb20e16beff91bd396eea52b936316ab70b
web-tech-76 <webtec76@gmail.com>
2024-03-02 15:12:39 -0500

    new module batch2

---
+++ batch-2/src/main/java/batch2/app/Batch2Application.java
@@ -0,0 +1,13 @@
+package batch2.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Batch2Application.class, args);
+    }
+
+}
---
+++ batch-2/src/test/java/batch2/app/Batch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package batch2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
